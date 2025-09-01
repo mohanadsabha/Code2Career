59f96c7a5305d73eaf6c2b5eb087a8434f93b42b
Mohanad Abusabha <eng.mohanadsabha@gmail.com>
2025-09-02 00:14:44 +0300

    Intitialized project

---
+++ Task7/src/server.ts
@@ -0,0 +1,42 @@
+import express, { Request, Response, NextFunction } from "express";
+import dotenv from "dotenv";
+import AppError from "./shared/error.type";
+import globalErrorHandler from "./shared/error.handler.js";
+import { userRouter } from "./users/user.routes";
+
+dotenv.config({ path: "./config.env" });
+
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGT EXCEPTION! Shutting down...", err);
+  process.exit(1);
+});
+
+const app = express();
+
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+
+// Routes
+app.use("/api/v1/users", userRouter);
+
+// Unhandled routes
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+
+// Global Error Handeling Middleware
+app.use(globalErrorHandler);
+
+const PORT = process.env.PORT || 3000;
+
+const server = app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}...`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION! Shutting down...");
+  console.log(err);
+  server.close(() => {
+    process.exit(1);
+  });
+});
