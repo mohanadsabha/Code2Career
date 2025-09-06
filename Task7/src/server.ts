8b9b3f3bb9e45263004573403125f6570c575005
Mohanad Abu Sabha <mohannad.naxol@gmail.com>
2025-09-06 16:36:28 +0300

    Fixed dotenv reading in jwt.util

--- Task7/src/server.ts
+++ Task7/src/server.ts
@@ -1,12 +1,10 @@
 import dotenv from "dotenv";
 dotenv.config({ path: "./config.env" });
-console.log(process.env.NODE_ENV);
 import express, { Request, Response, NextFunction } from "express";
 import AppError from "./shared/error.type";
 import globalErrorHandler from "./shared/error.handler.js";
 import { userRouter } from "./users/user.routes";
 import { authRouter } from "./auth/auth.router";
-console.log(process.env.NODE_ENV);
 
 process.on("uncaughtException", (err) => {
   console.log("UNCAUGT EXCEPTION! Shutting down...", err);
