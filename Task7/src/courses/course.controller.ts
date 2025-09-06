04d0691c2d735418aad77a450a108d71d8fb6fc8
Mohanad Abu Sabha <mohannad.naxol@gmail.com>
2025-09-06 23:39:45 +0300

    Implemented CRUD operations on Course module

---
+++ Task7/src/courses/course.controller.ts
@@ -0,0 +1,73 @@
+import { NextFunction, Request, Response } from "express";
+import { courseService } from "./course.service";
+import AppError from "../shared/error.type";
+import { HttpErrorStatus } from "../shared/http.status";
+
+class CourseController {
+  private courseService = courseService;
+  public getCourses(req: Request, res: Response, next: NextFunction) {
+    res.status(200).json({
+      success: true,
+      data: this.courseService.getCourses(),
+    });
+  }
+  public getCourse(
+    req: Request<{ id: string }>,
+    res: Response,
+    next: NextFunction
+  ) {
+    const course = this.courseService.getCourse(req.params.id);
+    if (!course) {
+      return next(new AppError("Course not found", HttpErrorStatus.NotFound));
+    }
+    res.status(200).json({
+      success: true,
+      data: course,
+    });
+  }
+  public addCourse(req: Request, res: Response, next: NextFunction) {
+    const { title, description } = req.body;
+    // const image = req.file; implement multer
+    const course = this.courseService.addCourse(title, description);
+    res.status(201).json({
+      success: true,
+      data: course,
+    });
+  }
+  public updateCourse(
+    req: Request<{ id: string }>,
+    res: Response,
+    next: NextFunction
+  ) {
+    const id = req.params.id;
+    if (!id)
+      return next(new AppError("ID required", HttpErrorStatus.BadRequest));
+
+    const { title, description } = req.body;
+    // const image = req.file; implement multer
+
+    const course = this.courseService.updateCourse(id, title, description);
+    if (!course) {
+      return next(new AppError("Course not found", HttpErrorStatus.NotFound));
+    }
+    res.status(200).json({
+      success: true,
+      data: course,
+    });
+  }
+
+  public deleteCourse(
+    req: Request<{ id: string }>,
+    res: Response,
+    next: NextFunction
+  ) {
+    if (!req.params.id)
+      return next(new AppError("ID required", HttpErrorStatus.BadRequest));
+
+    res.status(200).json({
+      success: this.courseService.deleteCourse(req.params.id),
+    });
+  }
+}
+
+export const courseController = new CourseController();
---
+++ Task7/src/courses/course.service.ts
@@ -0,0 +1,29 @@
+import { Course } from "./course.entity";
+import { courseRepository } from "./course.repository";
+
+class CourseService {
+  private repository = courseRepository;
+  public getCourses() {
+    return this.repository.findAll();
+  }
+  public getCourse(id: string): Course | undefined {
+    return this.repository.findById(id);
+  }
+  public addCourse(title: string, description: string, image?: string) {
+    return this.repository.create({ title, description, image });
+  }
+  public updateCourse(
+    id: string,
+    title: string,
+    description: string,
+    image?: string
+  ): Course | null {
+    return this.repository.update(id, { title, description, image });
+  }
+  public deleteCourse(id: string): boolean {
+    return this.repository.delete(id);
+  }
+  public checkOwnerShip() {}
+}
+
+export const courseService = new CourseService();
